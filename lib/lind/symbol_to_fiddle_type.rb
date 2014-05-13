457d84769d83cee91e14eb0f0482809ea90bcc1a
myun2 <myun2@nwhite.info>
2014-05-13 20:16:27 +0900

    fix case statement

--- lib/lind/symbol_to_fiddle_type.rb
+++ lib/lind/symbol_to_fiddle_type.rb
@@ -5,20 +5,12 @@ module Lind
       return sym unless sym.kind_of? Symbol
 
       case sym
-      when nil
-      when :non
-      when :none
-      when :v
-      when :void   then Fiddle::TYPE_VOID
-      when :c
-      when :char   then Fiddle::TYPE_CHAR
-      when :i
-      when :int    then Fiddle::TYPE_INT
-      when :l
-      when :long   then Fiddle::TYPE_LONG
-      when :short  then Fiddle::TYPE_SHORT
-      when :s
-      when :string then Fiddle::TYPE_VOIDP
+      when nil, :non, :none, :v, :void then Fiddle::TYPE_VOID
+      when :c, :char   then Fiddle::TYPE_CHAR
+      when :i, :int    then Fiddle::TYPE_INT
+      when :l, :long   then Fiddle::TYPE_LONG
+      when :short      then Fiddle::TYPE_SHORT
+      when :s, :string then Fiddle::TYPE_VOIDP
       end
     end
   end
