8be8d2ee48d26f20dbd1cd5cb0a00d092483e4c0
myun2 <myun2@nwhite.info>
2014-05-13 17:07:44 +0900

    lind_spec use mock library

--- spec/lind_spec.rb
+++ spec/lind_spec.rb
@@ -2,20 +2,22 @@
 require './lib/lind/of_fiddle_importer'
 
 describe Lind do
-  context "MessageBox API" do
-    let(:declare_module) do
-      module MessageBox
-        extend Lind::OfFiddleImporter
-
-        library "user32"
-        declare "MessageBox", ["void*", "const char*", "const char*", "int"], "int"
-      end
-    end
-
-    before do
-      declare_module
-    end
-
-    it { MessageBox.MessageBox(nil, "aa", "bb", 0).should eq 1 }
+  before do
+    `gcc -shared -o spec/mock.dll spec/mock_src/mock.c`
   end
+
+  let(:declare_module) do
+    module MockLib
+      extend Lind::OfFiddleImporter
+
+      library "./spec/mock"
+      declare "x_plus_y", ["int", "int"], "int"
+    end
+  end
+
+  before do
+    declare_module
+  end
+
+  it { MockLib.x_plus_y(4, 7).should eq 11 }
 end
